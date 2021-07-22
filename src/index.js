8346c13322957fdc583f0d0e9b055eb66a0317d2
Amine_Zerradi <dasileker@gmail.com>
2021-07-22 10:41:18 +0100

    add description + screenshot to the readme

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import Home from "./pages/Home";
 import App from './App';
 
 ReactDOM.render(
