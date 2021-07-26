960ad181b6cfaa6834c7c38adbacfc4d05eebf4b
Amine_Zerradi <dasileker@gmail.com>
2021-07-26 13:30:12 +0100

    add actions folder + APi files meal list + categories meal + ingredient

---
+++ src/actions/getCategories.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { CategoriesActionTypes } from './actionTypes';
+
+export const fetchCategoriesStart = () => ({
+  type: CategoriesActionTypes.FETCH_DATA_START,
+});
+
+export const fetchCategoriesSuccess = categories => ({
+  type: CategoriesActionTypes.FETCH_DATA_SUCCEESS,
+  categories,
+});
+
+export const fetchCategoriesFailure = error => ({
+  type: CategoriesActionTypes.FETCH_DATA_FAILURE,
+  error,
+});
+
+// eslint-disable-next-line arrow-body-style
+export const fetchCategoriesStartAsync = () => {
+  return dispatch => {
+    dispatch(fetchCategoriesStart());
+    axios
+      .get('https://www.themealdb.com/api/json/v1/1/categories.php')
+      .then(res => dispatch(fetchCategoriesSuccess(res.data.categories)))
+      .catch(error => dispatch(fetchCategoriesFailure(error)));
+  };
+};
