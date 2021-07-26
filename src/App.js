30db720e0d3f52e0347fb2a4f10d528c21f089af
Amine_Zerradi <dasileker@gmail.com>
2021-07-26 14:08:22 +0100

    add style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './containers/Home';
+import NavBar from './components/NavBar';
+import IngredientsList from './containers/IngredientsList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <NavBar />
+        <Route exact path="/" component={Home} />
+        <Route path="/ingredients/:id" component={IngredientsList} />
+      </Router>
     </div>
   );
 }
