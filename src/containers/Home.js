e277567c40b6735805dac9c51b7b528d68fd3207
Amine_Zerradi <dasileker@gmail.com>
2021-07-26 11:12:19 +0100

    refactored the home page

---
+++ src/containers/Home.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import Categories from '../components/Categories';
+import MealList from './MealList';
+import { fetchIngredientsStartAsync } from '../actions/getIngredients';
+
+const Home = () => {
+  const [state, setState] = useState('Beef');
+  const dispatch = useDispatch();
+
+  const handleCategoryChange = category => {
+    setState(category);
+  };
+
+  useEffect(() => {
+    dispatch(fetchIngredientsStartAsync());
+  }, []);
+
+  return (
+    <div>
+      <Categories onCategoryChange={handleCategoryChange} />
+      <MealList handleChange={handleCategoryChange} cat={state} />
+    </div>
+  );
+};
+export default Home;
